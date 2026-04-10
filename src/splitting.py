07705da2f822a6d627680c2309ed9b215127ffb1
Daniel Mai <getwhattt@gmail.com>
2026-04-11 01:49:29 +0700

    Add scaler

--- src/splitting.py
+++ src/splitting.py
@@ -1,9 +1,22 @@
 """
 Time-series splitting utilities.
 """
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
-def time_train_test_split(df, test_size=0.2):
-    split_index = int(len(df) * (1 - test_size))
-    train = df.iloc[:split_index]
-    test = df.iloc[split_index:]
+def time_train_test_split(df, split_date="2014-01-01"):
+    df = df.sort_index()
+    
+    train = df.loc[df.index < split_date]
+    test = df.loc[df.index >= split_date]
+    
     return train, test
+
+def scaler (train, test):
+    scaler = StandardScaler()
+    scaler.fit(train)
+    
+    train_scaled = pd.DataFrame(scaler.transform(train), index=train.index, columns=train.columns)
+    test_scaled = pd.DataFrame(scaler.transform(test), index=test.index, columns=test.columns)
+    
+    return train_scaled, test_scaled
